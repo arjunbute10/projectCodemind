91cfcc08942ddeb669e2699bded23adb18313d7d
arjunbute10 <124491024+arjunbute10@users.noreply.github.com>
2023-10-15 15:25:07 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+ firebase : {
+        apiKey: "AIzaSyD98F_aXKIkAC6fJS1m1dNNajN38GD_BGU",
+        authDomain: "loginusers-ebfea.firebaseapp.com",
+        databaseURL: "https://loginusers-ebfea-default-rtdb.firebaseio.com",
+        projectId: "loginusers-ebfea",
+        storageBucket: "loginusers-ebfea.appspot.com",
+        messagingSenderId: "51012336654",
+        appId: "1:51012336654:web:3c6145d6e2b44c188b905f"
+      }
+  };
+  
